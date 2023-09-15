afa3dc4b7582bf638999f71c688804a610249dcc
Jairo Miguel <78458400+jmitsuu@users.noreply.github.com>
2023-09-15 06:45:20 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ export default defineNuxtConfig({
   routeRules: {
     '/examples/*': { redirect: '/redirect-route' },
     '/modify-headers-route': { headers: { 'x-magic-of': 'nuxt and vercel' } },
-    '/spa': { ssr: true },
+    '/spa': { ssr: false },
   },
  
   
