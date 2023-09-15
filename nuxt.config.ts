d8a230aa2b2da6bf9926aba402949c6554d59635
Jairo Miguel <78458400+jmitsuu@users.noreply.github.com>
2023-09-15 16:14:51 -0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,16 @@ export default defineNuxtConfig({
 
   
   ],
-
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+     cssnano:
+       process.env.NODE_ENV === 'production'
+         ? { preset: ['default', { discardComments: { removeAll: true } }] }
+         : false, // disable cssnano when not in production
+    },
+ },
   googleFonts: {
     families: {
       Roboto: true,
